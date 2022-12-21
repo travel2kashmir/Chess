2275ca5560a76e1eea24ba844ab1652336735afe
ahtisam-t2k <77894449+ahtisam-t2k@users.noreply.github.com>
2022-12-21 17:59:21 +0530

    21-12-2022
    Fixed CSS

--- pages/index.js
+++ pages/index.js
@@ -30,20 +30,43 @@ function Index() {
   return (
 
     <div id="main-content"
-            className="bg-gray-200 px-4 pt-24 py-2 relative overflow-y-auto lg:ml-64">
-      <form >
-        <input
-          type="file"
-          accept="image/*"
-          onChange={(event) => setImage(event?.target?.files[0])}
-        /></form>
-      <button type="submit" onClick={handleSubmit}>Upload</button>
-      <Image
+            className="bg-gray-200  pt-10 py-2 relative overflow-y-auto ">
+     
+<div class="flex justify-center mt-8">
+    <div class="rounded-lg shadow-xl bg-gray-50 lg:w-3/2">
+        <div class="m-4">
+            <label class="inline-block mb-2 text-gray-500">Upload
+                Image(jpg,png,svg,jpeg)</label>
+            <div class="flex items-center justify-center w-full">
+                <label class="flex flex-col w-full h-32 border-4 border-dashed hover:bg-gray-100 hover:border-gray-300">
+                    <div class="flex flex-col items-center justify-center pt-7">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            class="w-12 h-12 text-gray-400 group-hover:text-gray-600" viewBox="0 0 20 20"
+                            fill="currentColor">
+                            <path fill-rule="evenodd"
+                                d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <p class="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
+                            Select a photo</p>
+                    </div>
+                    <input type="file" class="opacity-0"  onChange={(event) => setImage(event?.target?.files[0])} />
+                </label>
+            </div>
+        </div>
+        <div class="flex p-2 space-x-4">
+            
+            <button class="px-4 py-2 text-white bg-cyan-600 rounded shadow-xl" onClick={handleSubmit}>Upload</button>
+        </div>
+        <div class="flex p-4 space-x-8">
+        <Image
       src={previewImage}
       alt="Picture Demo"
-      width={200}
-      height={200}
-    />
+      width={700}
+      height={300}
+    /></div>
+    </div>
+</div>
     </div>
 
 
